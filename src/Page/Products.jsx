95f19f5ffcd9f46668c1321f864aa9960bd9fc0c
Pintu130 <pin2sharma907@gmail.com>
2024-09-17 21:06:04 +0530

    done

--- src/Page/Products.jsx
+++ src/Page/Products.jsx
@@ -28,7 +28,6 @@ const Products = () => {
   }));
   console.log();
   
-
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Product Gallery</h1>
